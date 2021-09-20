6a25b1c0db81d2b2a2d4102f85822c4fc8c6befc
J-Caques <j.caques@knights.ucf.edu>
2021-09-19 22:08:19 -0400

    revised exercise06

--- exercise06/src/main/java/exercise06/Solution06.java
+++ exercise06/src/main/java/exercise06/Solution06.java
@@ -1,6 +1,12 @@
 package exercise06;
 import java.util.Calendar;
 import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Jonathan Caques
+ */
+
 /*
    Prompt user "What is your age?" & "What age would you like to retire"
    Save user input through scanners
